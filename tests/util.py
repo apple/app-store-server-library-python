acd24ddf204285b6fb259d3a669aa609ad604a62
Alex Baker <alexander_j_baker@apple.com>
2024-02-21 18:27:43 -0800

    Bump cryptography and pyOpenSSL maximum version

--- tests/util.py
+++ tests/util.py
@@ -15,11 +15,11 @@ from appstoreserverlibrary.signed_data_verifier import SignedDataVerifier
 def create_signed_data_from_json(path: str) -> str:
     data = read_data_from_file(path)
     decoded_data = json.loads(data)
-    private_key = ec.generate_private_key(ES256).private_bytes(encoding=serialization.Encoding.PEM, format=serialization.PrivateFormat.TraditionalOpenSSL, encryption_algorithm=serialization.NoEncryption()).decode()
+    private_key = ec.generate_private_key(ec.SECP256R1).private_bytes(encoding=serialization.Encoding.PEM, format=serialization.PrivateFormat.TraditionalOpenSSL, encryption_algorithm=serialization.NoEncryption()).decode()
     return jwt.encode(payload=decoded_data, key=private_key, algorithm='ES256')
 
 def decode_json_from_signed_date(data: str) -> Dict[str, Any]:
-    public_key = ec.generate_private_key(ES256).public_key().public_bytes(encoding=serialization.Encoding.PEM, format=serialization.PublicFormat.SubjectPublicKeyInfo).decode()
+    public_key = ec.generate_private_key(ec.SECP256R1).public_key().public_bytes(encoding=serialization.Encoding.PEM, format=serialization.PublicFormat.SubjectPublicKeyInfo).decode()
     return decode_complete(jwt=data, key=public_key, algorithms=['ES256'], options={"verify_signature": False})
 
 def read_data_from_file(path: str) -> str:
@@ -31,10 +31,6 @@ def read_data_from_binary_file(path: str) -> str:
     full_path = os.path.join(path)
     with open(full_path, mode='rb') as test_file:
         return test_file.read()
-
-class ES256(ec.EllipticCurve):
-    name="prime256v1"
-    key_size = 256
 
 def get_signed_data_verifier(env: Environment, bundle_id: str, app_apple_id: int = 1234) -> SignedDataVerifier:
     verifier = SignedDataVerifier([read_data_from_binary_file('tests/resources/certs/testCA.der')], False, env, bundle_id, app_apple_id)
